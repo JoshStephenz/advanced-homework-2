9db5106b59629b20a975384d0b1c07f3352c6b20
JoshStephenz <joshstephensla@gmail.com>
2017-05-02 13:41:11 -0500

    Advanced Homework 2 w/Fixed TopNav

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,17 +1,49 @@
 import React from "react";
 
-function TopNav() {
-  return (	<div className="top-nav">
+function TopNav(props) {
+  return (
+    <div className="top-nav">
       <ul>
-        <li><a href="carlights.html">car lights</a></li>
-        <li><a href="carwheels.html">Car wheels</a></li>
-        <li><a href="carbumpers.html">car bumpers</a></li>
-        <li><a href="caradsystem.html">car audiosystem</a></li>
-        <li><a href="truckbumpers.html">Truck bumpers</a></li>
-        <li><a href="contact.html">Feedback</a></li>
+        <li><a href="#" onClick={
+          (e) => {
+          e.preventDefault();
+          props.changeCategory("headlights");
+          }
+        }>Car Lights</a></li>
+        <li><a href="#" onClick={
+          (e) => {
+            e.preventDefault();
+            props.changeCategory("tires");
+          }
+        }>Car Tires</a></li>
+        <li><a href="#" onClick={
+          (e) => {
+            e.preventDefault();
+            props.changeCategory("bumpers");
+          }
+        }>Car Bumpers</a></li>
+        <li><a href="#" onClick={
+          (e) => {
+            e.preventDefault();
+            props.changeCategory("audio");
+          }
+        }>Car Audio</a></li>
+        <li><a href="#" onClick={
+          (e) => {
+            e.preventDefault();
+            props.changeCategory("engine");
+          }
+        }>Engine</a></li>
+        <li><a href="#" onClick={
+          (e) => {
+            e.preventDefault();
+            alert('Feedback received');
+          }
+        }>Feedback</a></li>
         <div className="clear"> </div>
       </ul>
-    </div>);
+    </div>
+  );
 }
 
 export default TopNav;
